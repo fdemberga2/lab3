b8180e06ff9a6b3a993498d771227856d581d979
Felix Miguel Embarga <fdemberga2@student.apc.edu.ph>
2023-03-02 13:25:09 +0800

    Inline css for header

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -2,7 +2,73 @@
 <html>
 <head>
 <link href="css/index.css" rel="stylesheet">
+<style>
+div#navbar{
+    position: fixed;
+    top: 0;
+    height: 16%;
+    width: 100%;
+    z-index: 3;
+    left: 0;
+    bottom: 5;
+}
+div#pageselector{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 3;
+    background-color: rgb(131, 94, 0);
+}
+li#button{
+    width: 100px;
+    height: 50px;
+    background-color: transparent;
+    display: inline-block;
+    z-index: 3;
+}
+
+li a#button{
+    display: block;
+    color: white;
+    width: auto;
+    padding: 20px 20px 0px 0px;
+    text-decoration: none;
+    position: absolute;
+    font-size: 18px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+li a#button:hover{
+    color: #eeff00;
+}
+img#picture{
+    width: 50px;
+    padding: 0px 40px 0px 0px;
+    color: transparent;
+    border-radius: 1%;
+}
+ul{
+    display: inline-block;
+    z-index: 3;
+}
+
+hr#header{
+    opacity: 0.5;
+    color: white;
+    size: 4;
+}
+.button{
+position: fixed;
+right: 3%;
+top: 23%;
+width: auto;
+background-color: white;
+border-radius: 3%;
+text-align: center;
+}
+</style>
 </head>
+<style></style>
 <body>
     <div id="navbar" id="top">
         <div id="pageselector">
