54e74ccf482c7a93accb6edd0a13f3a48d36dc26
Felix Miguel Embarga <fdemberga2@student.apc.edu.ph>
2023-03-05 15:34:59 +0800

    CSS on form Fix Attempt 2

--- ci4/app/Controllers/Guest.php
+++ ci4/app/Controllers/Guest.php
@@ -11,7 +11,7 @@ class Guest extends BaseController
             'title' => 'Masterclass',
         ];
         return view('templates/header', $data)
-             . view('masterclass')
+             . view('pages/Masterclass')
              . view('templates/footer');
     }
     public function create()
@@ -22,7 +22,7 @@ class Guest extends BaseController
         if (! $this->request->is('post')) {
             // The form is not submitted, so returns the form.
             return view('templates/header', ['title' => 'Join The Masterclass'])
-                . view('Validation')
+                . view('pages/Validation')
                 . view('templates/footer');
         }
 
@@ -38,7 +38,7 @@ class Guest extends BaseController
         ])) {
             // The validation fails, so returns the form.
             return view('templates/header', ['title' => 'Join The Masterclass'])
-                . view('Validation')
+                . view('pages/Validation')
                 . view('templates/footer');
         }
 
@@ -53,7 +53,7 @@ class Guest extends BaseController
         ]);
 
         return view('templates/header', ['title' => 'Join The Masterclass'])
-            . view('masterclass')
+            . view('pages/Masterclass')
             . view('templates/footer');
     }
 }
